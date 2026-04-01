1d829e3cca618506d73d0081680071a8845da127
Tomás Peyronel <tpeyronel@gmail.com>
2026-04-01 12:38:54 -0300

    add freertos and task includes

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -25,7 +25,8 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "FreeRTOS.h"
+#include "task.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
