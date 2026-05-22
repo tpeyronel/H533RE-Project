07173b457bc84bfcd7156367eb8e9c223d92c299
Tomás Peyronel <tpeyronel@gmail.com>
2026-05-22 15:37:40 -0300

    add motor control pins, rename motor pwm pins

--- Core/Src/gpio.c
+++ Core/Src/gpio.c
@@ -48,11 +48,21 @@ void MX_GPIO_Init(void)
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, MOTOR_B_IN2_Pin|MOTOR_B_IN1_Pin|MOTOR_A_IN2_Pin|MOTOR_A_IN1_Pin, GPIO_PIN_RESET);
+
   /*Configure GPIO pin : USER_BUTTON_Pin */
   GPIO_InitStruct.Pin = USER_BUTTON_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(USER_BUTTON_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : MOTOR_B_IN2_Pin MOTOR_B_IN1_Pin MOTOR_A_IN2_Pin MOTOR_A_IN1_Pin */
+  GPIO_InitStruct.Pin = MOTOR_B_IN2_Pin|MOTOR_B_IN1_Pin|MOTOR_A_IN2_Pin|MOTOR_A_IN1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
   /* EXTI interrupt init*/
   HAL_NVIC_SetPriority(EXTI13_IRQn, 5, 0);
--- Core/Src/tim.c
+++ Core/Src/tim.c
@@ -379,7 +379,7 @@ void HAL_TIM_MspPostInit(TIM_HandleTypeDef* timHandle)
     PA0     ------> TIM2_CH1
     PA1     ------> TIM2_CH2
     */
-    GPIO_InitStruct.Pin = PWM_MOTOR_A_Pin|PWM_MOTOR_B_Pin;
+    GPIO_InitStruct.Pin = MOTOR_A_PWM_Pin|MOTOR_B_PWM_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
